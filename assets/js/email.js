c63b12604194b85fb0d9509fecd86f61142162aa
Nilesh <nileshhaldar98@gmail.com>
2022-12-21 19:36:12 +0530

    resume new window

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,31 +1,36 @@
-let send = document.getElementById("csend");
-send.addEventListener('click', function(e){
-e.preventDefault();
-let uname = document.getElementById("cname").value;
-let email = document.getElementById("cemail").value;
-let message = document.getElementById("cmessage").value;
-Email.send({
-    Host: "smtp.gmail.com",
-    Username: "nileshhaldar123@gmail.com",
-    Password: "meyjuuvupvfvguja",
-    To:'nileshhaldar98@gmail.com',
-    From: email,
-    Subject: "Sending Email from portfolio",
-    Body: message,
-    Attachments: [
-    {
-        name: "File_Name_with_Extension",
-        path: "Full Path of the file"
-    }]
-})
-    .then(function (message) {
-    alert("Mail has been sent successfully To Nilesh Haldar"),
-    document.getElementById("Contact-Form").reset();
-    });
-});
+// let send = document.getElementById("csend");
+// send.addEventListener('click', function(e){
+// e.preventDefault();
+// let uname = document.getElementById("cname").value;
+// let email = document.getElementById("cemail").value;
+// let message = document.getElementById("cmessage").value;
+// Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "nileshhaldar123@gmail.com",
+//     Password: "E2203274DFE2BD9C3483943BD60B7554F6A0",
+//     To:'nileshhaldar98@gmail.com',
+//     From: email,
+//     Subject: "Sending Email from portfolio",
+//     Body: message,
+//     Attachments: [
+//     {
+//         name: "File_Name_with_Extension",
+//         path: "Full Path of the file"
+//     }]
+// })
+//     .then(function (message) {
+//     alert("Mail has been sent successfully To Nilesh Haldar"),
+//     document.getElementById("Contact-Form").reset();
+//     });
+// });
 
 	
 	
 	
+
+
+
+
+
 
 
